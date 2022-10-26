50b3dde64c87cb840f5fa72722f9e63e73d4c82c
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-10-27 00:50:41 +0300

    add mouseover event handler for text on the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 //Business logic
 function randomColor() {
-  const h = ((Math.random())*100).toFixed();
+  const h = ((Math.random()) * 100).toFixed();
   const s = 50;
   const l = 50;
-    return `hsl(${h}deg, ${s}%, ${l}%)`;
+  return `hsl(${h}deg, ${s}%, ${l}%)`;
 }
 window.onload = function () {
-  const newColor = randomColor();
+
   let body = window.document.querySelector("body");
-  body.onmouseover = function() {
-  body.style.backgroundColor = newColor;
+  body.onmouseover = function () {
+    const newColor = randomColor();
+    body.style.backgroundColor = newColor;
+    const s = 0;
+    const l = 100;
+    textColor = randomColor();
+    body.style.color = textColor;
   };
-    
+  body.onclick = function () {
+    body.scrollTo({
+      top: 1000,
+      left: 150,
+      behavior: 'smooth'
+    });
+  };
 };
